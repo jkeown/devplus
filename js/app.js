1c2dedc34b9320f5376a19d0d2b56267831b809a
jkeown <jd.keown@yahoo.com>
2018-12-10 23:04:11 -0800

    initial commit with content

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Nav Mobile Menu
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge:'right'});
+  });
+
+  // TRY USING DROPDOWN IN PLACE OF SIDENAV
