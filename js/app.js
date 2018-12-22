13c15b72318a458336f824b42df1101893ea2766
jkeown <jd.keown@yahoo.com>
2018-12-22 03:27:33 -0800

    contact form budget options

--- js/app.js
+++ js/app.js
@@ -4,4 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, {edge:'right'});
   });
 
+// Contact Form Budget Options
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
+
   // TRY USING DROPDOWN IN PLACE OF SIDENAV
