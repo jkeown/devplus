099cbcd1386ba1348b1ce37529ad854290fb46c6
jkeown <jd.keown@yahoo.com>
2018-12-22 11:38:59 -0800

    solved sidenav not working

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 // Nav Mobile Menu
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {edge:'right'});
-  });
+  let elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems, {edge:'right'});
+});
 
 // Contact Form Budget Options
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
+  let elems = document.querySelectorAll('select');
+  M.FormSelect.init(elems);
+});
+
+// Scrollspy
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.scrollspy');
+  M.ScrollSpy.init(elems,{});
 });
 
   // TRY USING DROPDOWN IN PLACE OF SIDENAV
