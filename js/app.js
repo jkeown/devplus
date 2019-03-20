198025963c97607a62f318b6002d60e6a98c7cc7
jkeown <jd.keown@yahoo.com>
2019-03-19 22:23:59 -0700

    animation not supported on safari

--- js/app.js
+++ js/app.js
@@ -16,35 +16,5 @@ document.addEventListener('DOMContentLoaded', function() {
   M.ScrollSpy.init(elems,{});
 });
 
-// Services animation
-{
-  const elems = document.querySelectorAll('.animate');
-
-  const config = {
-    root: null,
-    rootMargin: "-50px 0px -50px 0px",
-    threshold: [1.0]
-  };
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // console.log('elem in view');
-        entry.target.classList.add('scale-up');
-        // observer.unobserve(entry.target);
-      }
-      else {
-        console.log('elem out of view');
-        entry.target.classList.remove('scale-up');
-      }
-    });
-  }, config);
-
-  elems.forEach(elem => {
-    observer.observe(elem);
-  });
-
-}
-
 // set copyright current year
 document.getElementById('currentYear').textContent = new Date().getFullYear();
