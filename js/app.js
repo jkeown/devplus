a48217324e6a100ef9758651e4e402828f5df537
jkeown <jd.keown@yahoo.com>
2019-04-15 13:19:33 -0700

    landscape media query, nav scroll css

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // nav scroll color change
 window.onscroll = () => {
   const nav = document.querySelector('nav');
-  if(this.scrollY <= 10) nav.classList.remove('scroll-color');
+  if(this.scrollY <= 10) nav.className = 'z-depth-0';
   else nav.className = 'scroll-color';
 };
 
